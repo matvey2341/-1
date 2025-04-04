f5d89bf8bec1b9751753d08b47b149384f421b2d
matvey2341 <skikhov55@gmail.com>
2025-04-04 09:19:54 +0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада: ");
+                double sum = Convert.ToDouble(Console.ReadLine());
+            if (sum < 100)
+            {
+                sum += sum * 0.05;
+        }
+            else if (sum <= 200)
+            {
+                sum += sum * 0.07;
+    }
+            else
+            {
+                sum += sum * 0.1;
+            }
+            Console.WriteLine("Сумму вклада после начисления процентов: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
